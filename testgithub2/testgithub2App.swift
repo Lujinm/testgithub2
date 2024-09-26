0e7b550f548fca7c3d83a436c1e5f7ec60e8cae8
lujin <lujinmohammed223@gmail.com>
2024-09-26 17:05:51 +0300

    Initial Commit

---
+++ testgithub2/testgithub2App.swift
@@ -0,0 +1,17 @@
+//
+//  testgithub2App.swift
+//  testgithub2
+//
+//  Created by lujin mohammed on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct testgithub2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
